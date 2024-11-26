a4efde89bd11d83e23f32bf23608e8a49614aa4d
Carlos <carlos.msv@hotmail.com>
2024-11-25 23:25:19 -0300

    feat: upsert transaction

---
+++ app/_components/upsert-transaction-dialog.tsx
@@ -0,0 +1,279 @@
+"use client"
+
+import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "./ui/dialog"
+import { DialogClose, DialogTitle } from "@radix-ui/react-dialog"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import { Input } from "./ui/input"
+import { MoneyInput } from "./money-input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
+import {
+  TRANSACTION_CATEGORY_OPTIONS,
+  TRANSACTION_PAYMENT_METHOD_OPTIONS,
+  TRANSACTION_TYPE_OPTIONS,
+} from "../_constants/transactions"
+import { DatePicker } from "./ui/date-picker"
+import { z } from "zod"
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { upsertTransaction } from "../_actions/upsert-transaction"
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "'Name' is required.",
+    })
+    .max(50),
+  amount: z
+    .number({
+      required_error: "'Value' is required.",
+    })
+    .positive({
+      message: "'Value' must be a positive number.",
+    }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "'Type' is required.",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "'Category' is required.",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "'Payment Method' is required.",
+  }),
+  date: z.date({
+    required_error: "'Date' is required.",
+  }),
+})
+
+type FormSchema = z.infer<typeof formSchema>
+
+interface UpsertTransactionDialogProps {
+  defaultValues?: FormSchema
+  transactionId?: string
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+}
+
+const UpsertTransactionDialog = ({
+  defaultValues,
+  transactionId,
+  isOpen,
+  setIsOpen,
+}: UpsertTransactionDialogProps) => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues ?? {
+      name: "",
+      amount: 0,
+      type: TransactionType.EXPENSE,
+      category: TransactionCategory.OTHER,
+      paymentMethod: TransactionPaymentMethod.CASH,
+      date: new Date(),
+    },
+  })
+
+  const onSubmit = async (data: FormSchema) => {
+    try {
+      await upsertTransaction({ ...data, id: transactionId })
+      setIsOpen(false)
+      form.reset()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const isUpdate = Boolean(transactionId)
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(open) => {
+        setIsOpen(open)
+        if (!open) {
+          form.reset()
+        }
+      }}
+    >
+      <DialogTrigger asChild></DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{isUpdate ? "Update" : "Add"} Transaction</DialogTitle>
+          <DialogDescription>Insert the information below</DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Type the name..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <MoneyInput
+                      placeholder="Type the amount..."
+                      value={field.value}
+                      onValueChange={({ floatValue }) =>
+                        field.onChange(floatValue)
+                      }
+                      onBlur={field.onBlur}
+                      disabled={field.disabled}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Type</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a type for the transaction" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_TYPE_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="paymentMethod"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Payment Method</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a payment method for the transaction" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_PAYMENT_METHOD_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a category for the transaction" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {TRANSACTION_CATEGORY_OPTIONS.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <DatePicker value={field.value} onChange={field.onChange} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="outline">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="submit">{isUpdate ? "Update" : "Add"}</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default UpsertTransactionDialog
--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -4,7 +4,8 @@ import { Transaction } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import TransactionTypeBadge from "../_components/type-badge"
 import { Button } from "@/app/_components/ui/button"
-import { PencilIcon, TrashIcon } from "lucide-react"
+import { TrashIcon } from "lucide-react"
+import EditTransactionButton from "../_components/edit-transaction-button"
 
 export const convertToTitleCase = (str: string) => {
   return str
@@ -59,16 +60,10 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "actions",
     header: "Actions",
-    cell: () => {
+    cell: ({ row: { original: transaction } }) => {
       return (
         <div className="space-x-1">
-          <Button
-            variant="ghost"
-            size={"icon"}
-            className="text-muted-foreground"
-          >
-            <PencilIcon />
-          </Button>
+          <EditTransactionButton transaction={transaction} />
           <Button
             variant="ghost"
             size={"icon"}
