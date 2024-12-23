e1ec9acb15b025e68128c7638b6e3f5ff45da54a
Carlos <carlos.msv@hotmail.com>
2024-12-23 02:12:56 -0300

    style: removed double padding

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -19,7 +19,7 @@ const TransactionsPage = async () => {
     <>
       <Navbar />
       <div className="space-y-6 overflow-hidden p-6">
-        <div className="flex w-full items-center justify-between p-6">
+        <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transactions</h1>
           <AddTransactionButton />
         </div>
